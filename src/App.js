e9477c2ce104e3f680b00161f7d295f4f3e0061a
adityavikram797 <adityavikram797@gmail.com>
2022-10-15 18:31:05 +0530

    started with widgets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Accordian from "./components/accordian";
+
+const items = [
+  {
+    title:"what is react?",
+    content:"React is js library"
+  },
+  {
+    title:"what is Js?",
+    content:"js is single threaded , synchronous language"
+  },
+  {
+    title:"what is mongoDb?",
+    content:"mongoDb is No-sql database"
+  }
+]
+
+const App = () =>{
+  return(
+    <>
+      <div>
+      <Accordian items = {items}/>
+
+      </div>
+      
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/accordian/index.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { useState } from "react"
+
+const Accordian = (props) =>{
+    const {items} = props
+    const [active, setActive] = useState(null)
+    const [count,setCount] = useState(0)
+    const called = (index) =>{
+        console.log('clicked',index)
+     
+        setActive(index)
+    }
+    const con = items.map((item,index) => {
+        const status = index === active?'active':''
+        return <React.Fragment key={item.title}>
+            <div className={'title' +status} onClick={()=>{called(index)}}>
+                <i className="dropdown icon"></i>
+                {item.title}
+            </div>
+            <div className={"content" +status}>
+                <h5>{item.content}</h5>
+            </div>
+        </React.Fragment>
+    })
+    return(
+        <>
+        <br/>
+         <div className="ui styled accordion">
+         {con}
+         
+
+         </div>
+         
+         
+            
+        </>
+    )
+}
+
+export default Accordian
